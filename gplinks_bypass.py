e72f2994d1c948d6d1f927a2bc8b5c61249497ef
Yuuki <xcscxr@gmail.com>
2022-04-09 09:42:27 +0530

    Update (Fixed #3)

--- gplinks_bypass.py
+++ gplinks_bypass.py
@@ -1,49 +1,43 @@
 import time
-import requests
 import cloudscraper
-from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
+# eg: https://gplinks.co/XXXX
+url = ""
 
-url = "" # eg: https://gplinks.co/XXXX
+# =======================================
 
-# ==============================================
+def gplinks_bypass(url: str):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
 
-def gplinks_bypass(url):
-    scraper = cloudscraper.create_scraper(allow_brotli=False)
-    res = scraper.get(url)
-    
-    h = { "referer": res.url }
-    res = scraper.get(url, headers=h)
-    
-    bs4 = BeautifulSoup(res.content, 'lxml')
+    res = client.head(url)
+    header_loc = res.headers['location']
+    param = header_loc.split('postid=')[-1]
+    req_url = f'{p.scheme}://{p.netloc}/{param}'
+
+    p = urlparse(header_loc)
+    ref_url = f'{p.scheme}://{p.netloc}/'
+
+    h = { 'referer': ref_url }
+    res = client.get(req_url, headers=h, allow_redirects=False)
+
+    bs4 = BeautifulSoup(res.content, 'html.parser')
     inputs = bs4.find_all('input')
     data = { input.get('name'): input.get('value') for input in inputs }
 
     h = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'x-requested-with': 'XMLHttpRequest'
+        'referer': ref_url,
+        'x-requested-with': 'XMLHttpRequest',
     }
-    
-    time.sleep(10) # !important
-    
-    p = urlparse(url)
-    final_url = f'{p.scheme}://{p.netloc}/links/go'
-    res = scraper.post(final_url, data=data, headers=h).json()
+    time.sleep(10)
+    res = client.post(final_url, headers=h, data=data)
+    try:
+        return res.json()['url'].replace('\/','/')
+    except: return 'Something went wrong :('
 
-    return res
-
-# ==============================================
+# =======================================
 
 print(gplinks_bypass(url))
-
-# ==============================================
-
-'''
-SAMPLE OUTPUT:
-{
-    'status': 'success', 
-    'message': 'XXXXXX', 
-    'url': 'XXXX' (Bypassed URL)
-}
-'''
