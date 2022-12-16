ac12079b6f26ee0ce5b19283751fdcde5eecfe8c
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-12-16 11:23:08 +0530

    Update gplinks_bypass.py

--- gplinks_bypass.py
+++ gplinks_bypass.py
@@ -1,41 +1,39 @@
+
+
 import time
-import cloudscraper
-from urllib.parse import urlparse
+
 from bs4 import BeautifulSoup
 
+import requests
 # eg: https://gplinks.co/XXXX
-url = ""
+url = "https://gplinks.co/Z94r6"
 
 # =======================================
 
 def gplinks_bypass(url: str):
-    client = cloudscraper.create_scraper(allow_brotli=False)
-    p = urlparse(url)
-    final_url = f'{p.scheme}://{p.netloc}/links/go'
-
-    res = client.head(url)
-    header_loc = res.headers['location']
+ 
+ url = url[:-1] if url[-1] == '/' else url
+ token = url.split("/")[-1]
     
-    p = urlparse(header_loc)
-    ref_url = f'{p.scheme}://{p.netloc}/'
+ domain ="https://gplinks.co/"
+ referer = "https://mynewsmedia.co/"
 
-    h = { 'referer': ref_url }
-    res = client.get(url, headers=h, allow_redirects=False)
+    
+ client = requests.Session()
+ vid = client.get(url, allow_redirects= False).headers["Location"].split("=")[-1]
+ url = f"{url}/?{vid}"
 
-    bs4 = BeautifulSoup(res.content, 'html.parser')
-    inputs = bs4.find_all('input')
-    data = { input.get('name'): input.get('value') for input in inputs }
+ response = client.get(url, allow_redirects=False)
+ soup = BeautifulSoup(response.content, "html.parser")
+    
+    
+ inputs = soup.find(id="go-link").find_all(name="input")
+ data = { input.get('name'): input.get('value') for input in inputs }
+    
 
-    h = {
-        'referer': ref_url,
-        'x-requested-with': 'XMLHttpRequest',
-    }
-    time.sleep(10)
-    res = client.post(final_url, headers=h, data=data)
-    try:
-        return res.json()['url'].replace('\/','/')
-    except: return 'Something went wrong :('
-
-# =======================================
+ time.sleep(5)
+ headers={"x-requested-with": "XMLHttpRequest"}
+ bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
+ return bypassed_url
 
 print(gplinks_bypass(url))
